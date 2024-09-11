1ed3bfe6218500b31d425a817c50b0555456dbe1
hamsa <hamsa@Hamsa.mynetworksettings.com>
2024-09-10 20:36:56 -0400

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,5 +5,9 @@ public class GitHomework {
 	public static void main (String [] args) {
 		System.out.println("Batch 12: Git Class Lecture");
 	}
+	
+	public static void  add(int a, int b) {
+		System.out.println(a + b);
+	}
 
 }
