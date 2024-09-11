f743a2a9306d86be2d47648953981070469f1396
hamsa <hamsa@Hamsa.mynetworksettings.com>
2024-09-10 22:06:19 -0400

    ad

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,13 +2,4 @@ package homework;
 
 public class GitHomework {
 	
-	public void display() {
-		System.out.println("Hiba Faisal"); 
-	}
-	/**
-          This is coming from GitHub
-          Java is fun
-          Git is Easy
-          API is not fun
-	*/
 }
