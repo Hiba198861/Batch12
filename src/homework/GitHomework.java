38b863a9182b4374bb31c8a1a5bf9235e3556f3b
hamsa <hamsa@Hamsa.mynetworksettings.com>
2024-09-10 21:15:19 -0400

    push

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,48 +2,5 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main (String [] args) {
-		System.out.println("Batch 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-	}
-	
-	public static void  add(int a, int b) {
-		System.out.println(a + b);
-
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-	}
-	
-	public static void gender (char a, char b) {
-		System.out.println("a,b");
-
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-	}
-	
-	
-	public static void level (char c, char m) {
-		System.out.println("c,m");
-
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-		
-	}
-	
-	
-	public static void doors (int x, int y) {
-		System.out.println(x +y);
-	}
-	
-	
-	
-
-	
-	
 
 }
